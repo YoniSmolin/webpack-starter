8a98939abd2959c360316886a2e4df2b9e4a3332
Yoni Smolin <yonis@microsoft.com>
2018-12-24 18:01:35 +0200

    1. Add Application Insights snippet, enabling fetch tracking in the config.
    2. Add a window.fetch() call in index.js
    (don't forget to set your instrumentation key value)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+let url = "http://demo5282476.mockable.io";
+
+let promise = window.fetch(url);
+
+promise.then(
+    data => console.log('success: ', data),
+    error => console.log('error: ', error)
+);
