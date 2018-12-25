cb63d7a9339c182d55e6b64cf55e237afc14e332
Yoni Smolin <yonis@microsoft.com>
2018-12-25 09:20:25 +0200

    1. De-noise Application Insights by limiting trace config
    2. Make sure that window.fetch() response is not being cached by adding appropriate headers to the request

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,7 +4,16 @@ console.log('webpack starterkit');
 
 let url = "http://demo5282476.mockable.io";
 
-let promise = window.fetch(url);
+let headers = new Headers();
+headers.append('pragma', 'no-cache');
+headers.append('cache-control', 'no-cache');
+
+let init = {
+  method: 'GET',
+  headers: headers,
+};
+
+let promise = window.fetch(url, init);
 
 promise.then(
     data => console.log('success: ', data),
